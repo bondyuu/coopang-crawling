4847e06febf03d4b91c27e7c87d27d90e4d11c56
bondyuu <87316155+bondyuu@users.noreply.github.com>
2023-07-25 18:09:15 +0900

    first commit

---
+++ src/main/java/com/example/coopangcrawling/CoopangCrawlingApplication.java
@@ -0,0 +1,13 @@
+package com.example.coopangcrawling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoopangCrawlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoopangCrawlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/coopangcrawling/CoopangCrawlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coopangcrawling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoopangCrawlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
